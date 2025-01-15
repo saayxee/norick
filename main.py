06c9cd605a99c1c78e852926b456e8f4d58ec38a
Aayan Ali <syedaayanzaidi@hotmail.com>
2025-01-15 15:44:23 +0500

    Add ASCII art/logo.

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from termcolor import colored
+
+# Example ASCII Art Logo
+ascii_logo = f'''
+  
+  
+  ███╗   ██╗ ██████╗ ██████╗ ██╗ ██████╗██╗  ██╗
+  ████╗  ██║██╔═══██╗██╔══██╗██║██╔════╝██║ ██╔╝
+  ██╔██╗ ██║██║   ██║██████╔╝██║██║     █████╔╝ 
+  ██║╚██╗██║██║   ██║██╔══██╗██║██║     ██╔═██╗ 
+  ██║ ╚████║╚██████╔╝██║  ██║██║╚██████╗██║  ██╗
+  ╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝
+          {colored("", "white")}     
+  Never gonna give my streak up.          
+  {colored("https://github.com/saayxee/norick", "grey")} 
+
+'''
